f72af93d291c477875dab63115fc8a3dbb84fa67
Prannay Khosla <prannay.khosla@gmail.com>
2017-05-12 19:26:47 +0530

    update, GAN underway

---
+++ mnistGAN.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import numpy as np
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
+
+graph = tf.Graph()
+
+batch_size = 50
+
+with graph.as_default():
+	g_input = tf.placeholder(tf.float32,shape=(batch_size,embedding_size))
--- mnistnet.py
+++ mnistnet.py
@@ -17,7 +17,7 @@ train_input = tf.placeholder(tf.float32,shape=[None, 784])
 train_output = tf.placeholder(tf.float32, shape=[None, 10])
 dropout = tf.placeholder(tf.float32)
 
-weight1 = weight_variable([5,5,1,32],1.0)
+weight1 = weight_variable([5,5,1,8],1.0)
 bias1 = bias_variable([32])
 
 train_image = tf.reshape(train_input, [-1,28,28,1])
@@ -25,16 +25,21 @@ train_image = tf.reshape(train_input, [-1,28,28,1])
 hidden1 = tf.nn.relu(tf.nn.conv2d(train_image,weight1,strides=[1,1,1,1],padding='SAME') + bias1)
 hiddenp1 = tf.nn.max_pool(hidden1,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
 
-weight2 = weight_variable([5,5,32,64],0.5)
-bias2 = bias_variable([64])
+weight2 = weight_variable([5,5,8,32],0.5)
+bias2 = bias_variable([32])
 
 hidden2 = tf.nn.relu(tf.nn.conv2d(hiddenp1,weight2,strides=[1,1,1,1],padding='SAME') + bias2)
 hiddenp2 = tf.nn.max_pool(hidden2,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
 
+weight4 = weight_variable([2,2,32,64],0.5)
+bias4 = bias_variable([64])
+
+hiddenp3 = tf.nn.relu(tf.nn.conv2d(hiddenp2,weight4,strides=[1,1,1,1],padding='SAME') + bias4)
+
 weight3 = weight_variable([7*7*64,1024],1.0)
 bias3 = bias_variable([1024])
 
-hidden3 = tf.reshape(hiddenp2,[-1,7*7*64])
+hidden3 = tf.reshape(hiddenp3,[-1,7*7*64])
 hidden4 = tf.nn.relu(tf.matmul(hidden3,weight3) + bias3)
 
 hidden_drop = tf.nn.dropout(hidden4,dropout)
@@ -45,7 +50,7 @@ bias4 = bias_variable([10])
 output = tf.nn.softmax(tf.matmul(hidden_drop,weight4)  + bias4)
 learning_rate = tf.placeholder(tf.float32)
 cross_entropy = -tf.reduce_sum(train_output*tf.log(output))
-train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
+train_step = tf.train.GradientDescent(learning_rate).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(output,1),tf.argmax(train_output,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 saver = tf.train.Saver()
