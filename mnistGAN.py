d05c9cbe0aa3b3a466f2775362c8d0c6090a9bac
Prannay Khosla <prannay.khosla@gmail.com>
2017-05-13 00:48:36 +0530

    Code optimized

--- mnistGAN.py
+++ mnistGAN.py
@@ -137,7 +137,7 @@ with tf.Session(graph=graph) as session:
 		}
 		_,generator_loss = session.run([g_train,gen_loss],feed_dict=feed_dict)
 		_,disc_loss_val	 = session.run([d_train,disc_loss],feed_dict=feed_dict)
-		print("Losses: " + str(generator_loss.eval()) + " , " + str(disc_loss_val.eval()))
+		print("Losses: " + str(generator_loss) + " , " + str(disc_loss_val))
 		average_total_loss /= mnist.train.num_examples
 		print("Average Loss: " + str(average_total_loss))
 		average_total_loss = 0
