c70fcaeb5364710592009368d2202a7bd7569cf2
Prannay Khosla <prannay.khosla@gmail.com>
2017-05-13 16:15:38 +0530

    Fixed image sampling error

--- dcgan.py
+++ dcgan.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import scipy
 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
