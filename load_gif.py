d08d36a3bc5495cb74e214b099ab33df1f7e00c3
Prannay Khosla <prannay.khosla@gmail.com>
2017-05-15 17:38:04 +0530

    Rolling changes for loader

---
+++ load_gif.py
@@ -0,0 +1,20 @@
+import sys
+import subprocess
+
+f = open("../datasets/tgif-v1.0.tsv")
+fdata = list()
+for line in f.readlines():
+	data = line.split("\t")
+	title = data[0].split("/")[-1]
+	name = "gif_data/" + title.split(".")[0]
+	subprocess.call(["mkdir",name])
+	subprocess.call(["wget",data[0]],shell=True)
+	subprocess.call(["mv",title, name+"/"+title])
+	subprocess.call(["convert","-verbose","-coalesce",name+"/"+title,name+"/"+title.split(".")[0] + ".png"])
+	fdata.append(title.split(".")[0]+"\t"+data[1])
+f2 = open("data.list",mode="w")
+text = ""
+for t in fdata:
+	text += (t + "\n")
+f2.write(text)
+print("Written text/data")
