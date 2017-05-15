858e80bbe8565c00fb94542ca501d09d7a492898
Prannay Khosla <prannay.khosla@gmail.com>
2017-05-15 17:44:58 +0530

    Rolling

--- load_gif.py
+++ load_gif.py
@@ -1,14 +1,14 @@
 import sys
 import subprocess
 
-f = open("../datasets/tgif-v1.0.tsv")
+f = open("../dataset/tgif-v1.0.tsv")
 fdata = list()
 for line in f.readlines():
 	data = line.split("\t")
 	title = data[0].split("/")[-1]
 	name = "gif_data/" + title.split(".")[0]
 	subprocess.call(["mkdir",name])
-	subprocess.call(["wget",data[0]],shell=True)
+	subprocess.call(["wget " + data[0]],shell=True)
 	subprocess.call(["mv",title, name+"/"+title])
 	subprocess.call(["convert","-verbose","-coalesce",name+"/"+title,name+"/"+title.split(".")[0] + ".png"])
 	fdata.append(title.split(".")[0]+"\t"+data[1])
