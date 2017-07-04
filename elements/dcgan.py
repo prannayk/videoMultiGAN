a3507f0de7f1c56ea3469a0b6490e3fb8762130e
Prannay Khosla <prannay.khosla@gmail.com>
2017-07-04 19:13:39 +0530

    Made changes

--- elements/dcgan.py
+++ elements/dcgan.py
@@ -8,46 +8,45 @@ import numpy
 from gensim.models import KeyedVectors
 import scipy
 
-print("Loaded all keyed vectors required")
-# mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 # sys.path.append( '../util/')
 # from loader import *
-en_model = KeyedVectors.load_word2vec_format('/media/hdd/hdd/prannayk/temp.vec')
+# en_model = KeyedVectors.load_word2vec_format('/media/hdd/hdd/prannayk/temp.vec')
 
 
-path = sys.argv[1]
-count = int(sys.argv[2])
+# path = sys.argv[1]
+# count = int(sys.argv[2])
 
-dictionary =  {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9, 'the': 10, 'digit': 11, 'and': 12, 'are':13, 'bouncing': 14, 'moving':15, 'here':16, 'there':17, 'around':18, 'jumping':19, 'up':20, 'down':21, '.':22, 'is':23, 'left':24, 'right': 25}
-rev_dict = dict(zip(dictionary.values(), dictionary.keys()))
-def loader(path):
-	training_data = numpy.ndarray(shape=[50000,64,64,1], dtype=np.int32)
-	train_caption_data = list()
-	for i in range(count):
-		filename="%s/mnist_two_%d_gif.h5"%(path, count)
-		f = h5py.File(filename)
-		train_set = f['mnist_gif_train'].value
-		train_caption = f['mnist_captions_train']
-		train_caption = map(lambda x: map(lambda y : rev_dict[y],x),train_caption)
-		train_caption = map(lambda x: map(lambda y: en_model[y], x), train_caption)
-		shape = train_set.shape
-		train_images = np.array(map(lambda x: x.reshape(shape[3],shape[4],shape[2]), train_set[:,0]))
-		training_data[i*10000:i*10000 + 10000] = train_images
-		train_caption_data += train_caption
-	return training_data, np.array(train_caption_data)
+# dictionary =  {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9, 'the': 10, 'digit': 11, 'and': 12, 'are':13, 'bouncing': 14, 'moving':15, 'here':16, 'there':17, 'around':18, 'jumping':19, 'up':20, 'down':21, '.':22, 'is':23, 'left':24, 'right': 25}
+# rev_dict = dict(zip(dictionary.values(), dictionary.keys()))
+# def loader(path):
+# 	training_data = numpy.ndarray(shape=[50000,64,64,1], dtype=np.int32)
+# 	train_caption_data = list()
+# 	for i in range(count):
+# 		filename="%s/mnist_two_%d_gif.h5"%(path, count)
+# 		f = h5py.File(filename)
+# 		train_set = f['mnist_gif_train'].value
+# 		train_caption = f['mnist_captions_train']
+# 		train_caption = map(lambda x: map(lambda y : rev_dict[y],x),train_caption)
+# 		train_caption = map(lambda x: map(lambda y: en_model[y], x), train_caption)
+# 		shape = train_set.shape
+# 		train_images = np.array(map(lambda x: x.reshape(shape[3],shape[4],shape[2]), train_set[:,0]))
+# 		training_data[i*10000:i*10000 + 10000] = train_images
+# 		train_caption_data += train_caption
+# 	return training_data, np.array(train_caption_data)
 
-training_data, train_caption_data = loader(path)
+# training_data, train_caption_data = loader(path)
 batch_size = 100
-embedding_size = 180
-def generator():
-	global training_data, train_caption_data, embedding_size
-	t = np.random.randint(0,len(train_caption_data)-batch_size)
-	image_shape = reduce(lambda x,y : x*y , training_data[0].shape)
-	image_data = training_data[t:t+batch_size].reshape(batch_size, image_shape)
-	caption_data = np.mean(train_caption_data[t:t+batch_size],axis=1)
-	l = np.random.normal(scale=0.01, size=[batch_size, embedding_size])
-	return image_data, caption_data, l
+# embedding_size = 180
+# def generator():
+# 	global training_data, train_caption_data, embedding_size
+# 	t = np.random.randint(0,len(train_caption_data)-batch_size)
+# 	image_shape = reduce(lambda x,y : x*y , training_data[0].shape)
+# 	image_data = training_data[t:t+batch_size].reshape(batch_size, image_shape)
+# 	caption_data = np.mean(train_caption_data[t:t+batch_size],axis=1)
+# 	l = np.random.normal(scale=0.01, size=[batch_size, embedding_size])
+# 	return image_data, caption_data, l
 
 
 def save_visualization(X, nh_nw, save_path='./mnistimages/sample.jpg'):
@@ -75,6 +74,9 @@ class DCGAN():
 		self.dim2 = dim2
 		self.dim3 = dim3
 		self.dim4 = dim4
+		self.dim_2 = self.image_shape[0] // 2
+		self.dim_4 = self.image_shape[0] // 4
+		self.dim_8 = self.image_shape[0] // 8
 		self.dim_channel = dim_channel
 		self.device = device
 		self.image_size = reduce(lambda x,y : x*y, image_shape)
@@ -89,48 +91,33 @@ class DCGAN():
 			ystack = tf.reshape(classes, [self.batch_size,1, 1, self.num_class])
 			embedding = tf.concat(axis=1, values=[embedding, classes])
 			h1 = tf.layers.dense(embedding, units=self.dim1, activation=tf.tanh,
-				kernel_initializer=self.initializer,
-				kernel_regularizer=tf.contrib.layers.l2_regularizer,
-				activity_regularizer = tf.contrib.layers.l2_regularizer,
-				bias_regularizer=tf.contrib.layers.l2_regularizer,name='dense_1',
-				reuse=scope.reuse)
+				kernel_initializer=self.initializer, 
+				name='dense_1', reuse=scope.reuse)
 			h1_concat = tf.layers.batch_normalization(tf.concat(axis=1, values=[h1, classes]))
-			h2 = tf.layers.dense(h1_concat, units=8*8*self.dim2, activation=tf.tanh,
-				kernel_initializer=self.initializer,
-				kernel_regularizer=tf.contrib.layers.l2_regularizer,
-				activity_regularizer = tf.contrib.layers.l2_regularizer,
-				 bias_regularizer=tf.contrib.layers.l2_regularizer, name='dense_2',
-				reuse=scope.reuse)
+			h2 = tf.layers.dense(h1_concat, units=self.dim_8*self.dim_8*self.dim2, 
+				activation=tf.tanh, kernel_initializer=self.initializer,
+				name='dense_2',	reuse=scope.reuse)
 			h2_concat = tf.layers.batch_normalization(tf.concat(axis=3,
-				values=[tf.reshape(h2, shape=[self.batch_size,8,8,self.dim2]), 
-				ystack*tf.ones(shape=[self.batch_size, 8, 8, self.num_class])]))
+				values=[tf.reshape(h2, shape=[self.batch_size,self.dim_8,self.dim_8,self.dim2]), 
+				ystack*tf.ones(shape=[self.batch_size, self.dim_8, self.dim_8, self.num_class])]))
 			h3 = tf.layers.conv2d_transpose(inputs=h2_concat, filters = self.dim3, 
 				kernel_size=[4,4], strides=[2,2], padding='SAME', activation=tf.nn.relu,
 				kernel_initializer=self.initializer,
-				kernel_regularizer=tf.contrib.layers.l2_regularizer,
-				bias_regularizer=tf.contrib.layers.l2_regularizer,
-				activity_regularizer=tf.contrib.layers.l2_regularizer,
 				reuse=scope.reuse,name='conv_1')
             #print(h3.get_shape())
 			h3_concat = tf.layers.batch_normalization(tf.concat(axis=3,
-				values=[tf.reshape(h3, shape=[self.batch_size,16,16,self.dim3]), 
-				ystack*tf.ones(shape=[self.batch_size, 16, 16, self.num_class])]))
+				values=[tf.reshape(h3, shape=[self.batch_size,self.dim_4,self.dim_4,self.dim3]), 
+				ystack*tf.ones(shape=[self.batch_size, self.dim_4, self.dim_4, self.num_class])]))
 			h4 = tf.layers.conv2d_transpose(inputs=h3_concat, filters = self.dim4, 
 				kernel_size=[4,4], strides=[2,2], padding='SAME', activation=tf.nn.relu,
 				kernel_initializer=self.initializer,
-				kernel_regularizer=tf.contrib.layers.l2_regularizer,
-				bias_regularizer=tf.contrib.layers.l2_regularizer,
-				activity_regularizer=tf.contrib.layers.l2_regularizer,
 				reuse=scope.reuse,name="conv_2")
 			h4_concat = tf.layers.batch_normalization(tf.concat(axis=3,
-				values=[tf.reshape(h4, shape=[self.batch_size,32,32,self.dim4]), 
-				ystack*tf.ones(shape=[self.batch_size, 32, 32, self.num_class])]))
+				values=[tf.reshape(h4, shape=[self.batch_size,self.dim_2,self.dim_2,self.dim4]), 
+				ystack*tf.ones(shape=[self.batch_size, self.dim_2, self.dim_2, self.num_class])]))
 			h5 = tf.layers.conv2d_transpose(inputs=h4_concat, filters = self.dim_channel, 
 				kernel_size=[4,4], strides=[2,2], padding='SAME', activation=None,
 				kernel_initializer=self.initializer,
-				kernel_regularizer=tf.contrib.layers.l2_regularizer,
-				bias_regularizer=tf.contrib.layers.l2_regularizer,
-				activity_regularizer=tf.contrib.layers.l2_regularizer,
 				reuse=scope.reuse,name="conv_3")
 			return tf.layers.batch_normalization(h5)
 
