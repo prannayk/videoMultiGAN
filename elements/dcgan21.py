1cbbe5629cd24a22efb47813a6cbd754b87a62d2
Prannay Khosla <prannay.khosla@gmail.com>
2017-07-05 11:10:05 +0000

    Adding relevant images

--- elements/dcgan21.py
+++ elements/dcgan21.py
@@ -88,7 +88,7 @@ class DCGAN():
 				real_value = self.discriminate(real_image,classes,scope)
 			prob_real = tf.nn.sigmoid(real_value)
 			with tf.variable_scope("discriminator") as scope:
-				scope.reuse_variables
+				scope.reuse_variables()
 				fake_value = self.discriminate(g_image,classes,scope)
 			prob_fake = tf.nn.sigmoid(fake_value)
 			# d_cost = bce(real_value, tf.ones_like(real_value)) + bce(fake_value,tf.zeros_like(fake_value))
