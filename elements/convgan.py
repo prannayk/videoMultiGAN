8e6ae5d0b0127035d70ad9ec55cd911a6dc1517d
Prannay Khosla <prannay.khosla@gmail.com>
2017-07-07 18:02:16 +0000

    Adding relevant images

--- elements/convgan.py
+++ elements/convgan.py
@@ -229,7 +229,7 @@ num_class = 10
 gan = DCGAN(batch_size=batch_size, embedding_size=embedding_size, image_shape=[64,64,1], num_class=num_class)
 
 embedding, vector, real_image, d_loss, g_loss, prob_fake, prob_real = gan.build_model()
-session  = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=True))
+session  = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=False))
 # relevant weight list
 g_weight_list = [i for i in (filter(lambda x: x.name.startswith("gen"),tf.trainable_variables()))]
 d_weight_list = [i for i in (filter(lambda x: x.name.startswith("disc"),tf.trainable_variables()))]
