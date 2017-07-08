88ffd7042532e9e2e009b490305f8ad80371befa
Prannay Khosla <prannay.khosla@gmail.com>
2017-07-08 19:23:52 +0000

    Adding relevant images

--- elements/samplelstm.py
+++ elements/samplelstm.py
@@ -41,7 +41,7 @@ class DCGAN():
 		if not flag:
 			mean, vari = tf.nn.moments(X, 0, keep_dims=True)
 		elif local :
-			mean, vari = tf.nn.moments(X,1, keep_dims=True)
+			mean, vari = tf.nn.moments(X,[0,1], keep_dims=True)
 		else:
 			mean, vari = tf.nn.moments(X, [0,1,2], keep_dims=True)
 		return tf.nn.batch_normalization(X, mean, vari, offset=None, scale=None, variance_epsilon=1e-6,name=name)
