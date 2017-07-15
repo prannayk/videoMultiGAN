9b37b0e7f1532fae16807eae3896e288138ee253
Prannay Khosla <prannay.khosla@gmail.com>
2017-07-15 13:38:13 +0530

    Adding code

--- vaegan/frame_1_text_embedding.py
+++ vaegan/frame_1_text_embedding.py
@@ -365,11 +365,11 @@ def train_epoch(flag=False, initial=True):
 				placeholders['z_c'] : random_label(batch_size),
 				placeholders['z_t'] : np.random.normal(0,1,[batch_size, num_class_motion])
 			}
+			_, loss_val[0] = session.run([optimizers["discriminator"],losses["disc_image_discriminator"]], feed_dict=feed_dict)
 			if not initial:
-				_, loss_val[0] = session.run([optimizers["discriminator"],losses["disc_image_discriminator"]], feed_dict=feed_dict)
-			_, loss_val[1] = session.run([optimizers["code_discriminator"], losses["disc_image_classifier"]], feed_dict=feed_dict)
-			_, loss_val[2] = session.run([optimizers["text_discriminator"], losses["disc_text_classifier"]], feed_dict=feed_dict)
-			_, loss_val[3] = session.run([optimizers["style_discriminator"], losses["disc_style_classifier"]], feed_dict=feed_dict)
+				_, loss_val[1] = session.run([optimizers["code_discriminator"], losses["disc_image_classifier"]], feed_dict=feed_dict)
+				_, loss_val[2] = session.run([optimizers["text_discriminator"], losses["disc_text_classifier"]], feed_dict=feed_dict)
+				_, loss_val[3] = session.run([optimizers["style_discriminator"], losses["disc_style_classifier"]], feed_dict=feed_dict)
 
 		for _ in range(diter):
 			feed_list = generate(batch_size)
@@ -383,12 +383,10 @@ def train_epoch(flag=False, initial=True):
 				placeholders['z_c'] : random_label(batch_size),
 				placeholders['z_t'] : np.random.normal(0,1,[batch_size, num_class_motion])
 			}
+			_, loss_val[6] = session.run([optimizers["generator"], losses["generator_image"]], feed_dict=feed_dict)
 			if not initial :
-				_, loss_val[6] = session.run([optimizers["generator_reconstruction"],losses["reconstruction"]], feed_dict=feed_dict)
-			else:
-				_, loss_val[6] = session.run([optimizers["generator"], losses["generator_image"]], feed_dict=feed_dict)
-			_, loss_val[4] = session.run([optimizers["encoder"], losses["encoder"]], feed_dict=feed_dict)
-			_, loss_val[5] = session.run([optimizers["text_encoder"], losses["text_encoder"]], feed_dict=feed_dict)
+				_, loss_val[4] = session.run([optimizers["encoder"], losses["encoder"]], feed_dict=feed_dict)
+				_, loss_val[5] = session.run([optimizers["text_encoder"], losses["text_encoder"]], feed_dict=feed_dict)
 		count += 1
 		if count % 10 == 0 or flag:
 			print("%d:%d : "%(ep+1,run) + " : ".join(map(lambda x : str(x),loss_val)) + " " + str(time.time() - start_time))
