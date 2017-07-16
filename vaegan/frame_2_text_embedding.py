089901b882e303a47af49259e40e6859ff3dc341
Prannay Khosla <prannay.khosla@gmail.com>
2017-07-16 07:28:00 +0000

    Adding relevant images

--- vaegan/frame_2_text_embedding.py
+++ vaegan/frame_2_text_embedding.py
@@ -357,19 +357,19 @@ def generate(batch_size):
 			for r in range(2):
 				for j in range(3):
 					batch[i,34-(4*r):62-(4*r),34-(4*r):62-(4*r),j+(3*r)] = batch1[i]*l[j]
-					batch_gen[i, 26-(4*r):36-(4*r),26-(4*r):36-(4*r),j+(3*r)] = batch1[i]*l[j]
+					batch_gen[i, 26-(4*r):54-(4*r),26-(4*r):54-(4*r),j+(3*r)] = batch1[i]*l[j]
 		elif t==2 :
 			text_labels[i] = np.array([-1,-1,1,1])
 			for r in range(2):
 				for j in range(3):
 					batch[i,34-(4*r):62-(4*r),2+(4*r):30+(4*r),j+(3*r)] = batch1[i]*l[j]
-					batch_gen[i, 26-(4*r):36-(4*r),10+(4*r):38+(4*r),j+(3*r)] = batch1[i]*l[j]
+					batch_gen[i, 26-(4*r):54-(4*r),10+(4*r):38+(4*r),j+(3*r)] = batch1[i]*l[j]
 		else :
 			text_labels[i] = np.array([1,1,-1,-1])
 			for r in range(2):
 				for j in range(3):
 					batch[i,2+(4*r):30+(4*r),34-(4*r):62-(4*r),j+(3*r)] = batch1[i]*l[j]
-					batch_gen[i, 10+(4*r):38+(4*r),26-(4*r):36-(4*r),j+(3*r)] = batch1[i]*l[j]
+					batch_gen[i, 10+(4*r):38+(4*r),26-(4*r):54-(4*r),j+(3*r)] = batch1[i]*l[j]
 	return batch, batch_gen, batch_labels, text_labels
 
 def save_visualization(X, nh_nw=(8,8), save_path='../results/%s/sample.jpg'%(sys.argv[4])):
