7c79ae762ff910054afec0ba571185d90728907f
Prannay Khosla <prannay.khosla@gmail.com>
2017-07-18 11:03:00 +0000

    Adding relevant images

--- vaegan/frame_8_text_embedding.py
+++ vaegan/frame_8_text_embedding.py
@@ -424,7 +424,7 @@ def save_visualization(X, nh_nw=(16,8), save_path='../results/%s/sample.jpg'%(sy
 
 def morph(X):
 	batch_size = int(X.shape[0])
-	dim_channel = int(X.shape[-1]) // (frames*2)
+	dim_channel = int(X.shape[-1]) // (frames+2)
 	h,w = map(lambda x: int(x), X.shape[1:3])
 	img = np.zeros([2*frames*batch_size,h,w,dim_channel])
 	for i in range(batch_size):
