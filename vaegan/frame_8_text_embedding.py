d4096e2ed88dd127952aabc338aacf46d5073a5d
Prannay Khosla <prannay.khosla@gmail.com>
2017-07-18 12:32:52 +0000

    Adding relevant images

--- vaegan/frame_8_text_embedding.py
+++ vaegan/frame_8_text_embedding.py
@@ -15,8 +15,10 @@ class VAEGAN():
 		self.image_shape = image_shape
 		self.image_input_shape = list(image_shape)
 		self.image_create_shape = list(image_shape)
+		self.frames = frames_input
 		self.frames = frames
 		self.image_input_shape[-1] *= self.frames_input
+		self.image_create_shape[-1] *= self.frames
 		self.num_class_image = num_class_image
 		self.num_class_motion = num_class_motion
 		self.num_class = num_class_image
