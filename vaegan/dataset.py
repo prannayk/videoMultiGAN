a8fe1f31e2e92206cb35dbdec94d398abfc9b2da
Prannay Khosla <prannay.khosla@gmail.com>
2017-07-29 16:26:29 +0530

    Rolling with code

---
+++ vaegan/dataset.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+from generator import rot_generator as generate_rot
+from generator import text_generator as generate_text
+
+def create_dataset(batch_size, total_size, image_input_shape, image_create_shape, frames,type="rot"):
+    image_start = np.zeros(shape=[total_size] + image_input_shape)
+    image_gen = np.zeros(shape=[total_size] + image_create_shape)
+    image_labels = np.zeros(shape=[total_size, 13])
+    image_motion_labels = np.zeros(shape=[total_size, 4])
+    for i in range(total_size // batch_size):
+        if i % 1000 == 0:
+            print(i)
+        if type == "rot":
+            output_list = generate_rot(batch_size, frames)
+        elif type == "text":
+            output_list = generate_text(batch_size, frames)
+        else :
+            raise NotImplemented
+        image_start[i*batch_size : i*batch_size + batch_size] = output_list[0]
+        image_gen[i*batch_size:i*batch_size + batch_size] = output_list[1]
+        image_labels[i*batch_size:i*batch_size + batch_size] = output_list[2]
+        image_motion_labels[i*batch_size:i*batch_size + batch_size] = output_list[3]
+    dataset = {
+        "image_start" : image_start,
+        "image_gen" : image_gen,
+        "image_labels" : image_labels,
+        "image_motion_labels" : image_motion_labels
+    }
+    return dataset
+frames=2
+dataset = create_dataset(batch_size=8, total_size=6400, image_input_shape=[64,64,6], image_create_shape=[64,64,3*frames], frames=frames)
