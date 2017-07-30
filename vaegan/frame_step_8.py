09fe5d4ad19682e7c7d3971701ed5aff6ea2f889
Prannay Khosla <prannay.khosla@gmail.com>
2017-07-30 10:36:55 +0000

    Adding relevant images

--- vaegan/frame_step_8.py
+++ vaegan/frame_step_8.py
@@ -13,7 +13,7 @@ batch_size = 16
 class VAEGAN():
 	"""docstring for VAEGAN"""
 	def __init__(self, batch_size = 16, image_shape= [28,28,3], embedding_size = 128,
-			learning_rate = sys.argv[1:], motion_size = 4, num_class_motion=6, 
+			learning_rate = sys.argv[1:], motion_size = 5, num_class_motion=12, 
 			num_class_image=13, frames=2, frames_input=3, total_size = 64000, video_create=False):
 		self.batch_size = batch_size
 		self.image_shape = image_shape
