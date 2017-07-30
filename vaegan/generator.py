393f00afba2943a27caccdf49e1aa4489485245d
Prannay Khosla <prannay.khosla@gmail.com>
2017-07-30 10:23:59 +0000

    Adding relevant images

--- vaegan/generator.py
+++ vaegan/generator.py
@@ -21,11 +21,11 @@ def rot_generator(batch_size, frames):
 			text_labels[i][-1] *= random
 			text_labels[i][-2] *= random
 			for r in range(3):
-				batch2[i] = np.array(Image.fromarray(batch1[i] * 255.).rotate(r*rot, Image.BILINEAR).getdata() / 255.).reshape(28,28)
+				batch2[i] = (np.array(Image.fromarray(batch1[i] * 255.).rotate(r*rot, Image.BILINEAR).getdata()) / 255.).reshape(28,28)
 				for j in range(3):
 					batch[i,2+(random*r):30+(random*r),2+(random*r):30+(random*r),j+(3*r)] = batch2[i]*l[j]
 			for r in range(frames):
-				batch2[i] = np.array(Image.fromarray(batch1[i] * 255.).rotate((j+3)*rot, Image.BILINEAR).getdata() / 255.).reshape(28,28)
+				batch2[i] = (np.array(Image.fromarray(batch1[i] * 255.).rotate((j+3)*rot, Image.BILINEAR).getdata()) / 255.).reshape(28,28)
 				for j in range(3):
 					batch_gen[i, 10+(random*r):38+(random*r),10+(random*r):38+(random*r),j+(3*r)] = batch2[i]*l[j]
 		elif t==1 :
@@ -33,11 +33,11 @@ def rot_generator(batch_size, frames):
 			text_labels[i][-1] *= random
 			text_labels[i][-2] *= random
 			for r in range(2):
-				batch2[i] = np.array(Image.fromarray(batch1[i] * 255.).rotate(r*rot, Image.BILINEAR).getdata() / 255.).reshape(28,28)
+				batch2[i] = (np.array(Image.fromarray(batch1[i] * 255.).rotate(r*rot, Image.BILINEAR).getdata()) / 255.).reshape(28,28)
 				for j in range(3):
 					batch[i,34-(random*r):62-(random*r),34-(random*r):62-(random*r),j+(3*r)] = batch2[i]*l[j]
 			for r in range(frames):
-				batch2[i] = np.array(Image.fromarray(batch1[i] * 255.).rotate((j+3)*rot, Image.BILINEAR).getdata() / 255.).reshape(28,28)
+				batch2[i] = (np.array(Image.fromarray(batch1[i] * 255.).rotate((j+3)*rot, Image.BILINEAR).getdata()) / 255.).reshape(28,28)
 				for j in range(3):
 					batch_gen[i, 26-(random*r):54-(random*r),26-(random*r):54-(random*r),j+(3*r)] = batch2[i]*l[j]
 		elif t==2 :
@@ -45,11 +45,11 @@ def rot_generator(batch_size, frames):
 			text_labels[i][-1] *= random
 			text_labels[i][-2] *= random
 			for r in range(2):
-				batch2[i] = np.array(Image.fromarray(batch1[i] * 255.).rotate(r*rot, Image.BILINEAR).getdata() / 255.).reshape(28,28)
+				batch2[i] = (np.array(Image.fromarray(batch1[i] * 255.).rotate(r*rot, Image.BILINEAR).getdata()) / 255.).reshape(28,28)
 				for j in range(3):
 					batch[i,34-(random*r):62-(random*r),2+(random*r):30+(random*r),j+(3*r)] = batch2[i]*l[j]
 			for r in range(frames):
-				batch2[i] = np.array(Image.fromarray(batch1[i] * 255.).rotate((j+3)*rot, Image.BILINEAR).getdata() / 255.).reshape(28,28)
+				batch2[i] = (np.array(Image.fromarray(batch1[i] * 255.).rotate((j+3)*rot, Image.BILINEAR).getdata()) / 255.).reshape(28,28)
 				for j in range(3):
 					batch_gen[i, 26-(random*r):54-(random*r),10+(random*r):38+(random*r),j+(3*r)] = batch2[i]*l[j]
 		else :
@@ -57,11 +57,11 @@ def rot_generator(batch_size, frames):
 			text_labels[i][-1] *= random
 			text_labels[i][-2] *= random
 			for r in range(2):
-				batch2[i] = np.array(Image.fromarray(batch1[i] * 255.).rotate(r*rot, Image.BILINEAR).getdata() / 255.).reshape(28,28)
+				batch2[i] = (np.array(Image.fromarray(batch1[i] * 255.).rotate(r*rot, Image.BILINEAR).getdata()) / 255.).reshape(28,28)
 				for j in range(3):
 					batch[i,2+(random*r):30+(random*r),34-(random*r):62-(random*r),j+(3*r)] = batch2[i]*l[j]
 			for r in range(frames):
-				batch2[i] = np.array(Image.fromarray(batch1[i] * 255.).rotate((j+3)*rot, Image.BILINEAR).getdata() / 255.).reshape(28,28)
+				batch2[i] = (np.array(Image.fromarray(batch1[i] * 255.).rotate((j+3)*rot, Image.BILINEAR).getdata()) / 255.).reshape(28,28)
 				for j in range(3):
 					batch_gen[i, 10+(random*r):38+(random*r),26-(random*r):54-(random*r),j+(3*r)] = batch2[i]*l[j]
 	return batch, batch_gen, batch_labels, text_labels
