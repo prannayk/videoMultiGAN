c0c36a42bb3789601c98b8d284cf36a1ff2c8ec2
Prannay Khosla <prannay.khosla@gmail.com>
2017-07-31 11:49:45 +0000

    Adding relevant images

--- acrcn/process.py
+++ acrcn/process.py
@@ -2,7 +2,7 @@ import os
 from os.path import join, isfile
 import sys
 from subprocess import call
-
+from PIL import Image
 
 folders = ["boxing", "handclapping", "handwaving", "jogging", "running", "walking"]
 direc = "/media/hdd/hdd/prannayk/action_reaction/"
@@ -16,3 +16,10 @@ for folder in folders[:1] :
 		os.system("ffmpeg -i %s/%s.avi -vf fps=5 -f image2 %s/%s/%s-"%(path, filename, path, filename, filename) + "%03d.png ")
 		os.system("mv %s/%s-* %s/"%(path, filename, filename))
 		print("Done with %s"%(filename))
+		path_file = direc + folder + "/" + filename
+		images =[f for f in os.listdir(path_file) if isflie(join(path_file, f))]
+		images = images[:30]
+		frames = np.zeros([30, 84, 64])
+		for i,img in enumerate(images) : 
+			im = (np.array(Image("%s/%s"%(path, filename)).getdata()) / 255. ).resize([84,64, 3])
+			frames[i] = im
