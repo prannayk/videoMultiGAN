bd6ab119e25b7a2fdb485bbadd9f7e31318e6ea4
Prannay Khosla <prannay.khosla@gmail.com>
2017-07-31 12:07:08 +0530

    adding code

--- vaegan/generator.py
+++ vaegan/generator.py
@@ -64,11 +64,11 @@ def rot_generator(batch_size, frames):
 				batch2[i] = (np.array(Image.fromarray(batch1[i] * 255.).rotate((j+3)*rot, Image.BILINEAR).getdata()) / 255.).reshape(28,28)
 				for j in range(3):
 					batch_gen[i, 26-(random*r):54-(random*r),10+(random*r):38+(random*r),j+(3*r)] = batch2[i]*l[j]
-        else :
-            text_labels[i] = np.array([rot, 1,1,-1,-1])
-            text_labels[i][-1] *= random
-            text_labels[i][-2] *= random
-            for r in range(3):
+		else :
+			text_labels[i] = np.array([rot, 1,1,-1,-1])
+			text_labels[i][-1] *= random
+			text_labels[i][-2] *= random
+			for r in range(3):
 				batch2[i] = (np.array(Image.fromarray(batch1[i] * 255.).rotate(r*rot, Image.BILINEAR).getdata()) / 255.).reshape(28,28)
 				for j in range(3):
 					batch[i,2+(random*r):30+(random*r),34-(random*r):62-(random*r),j+(3*r)] = batch2[i]*l[j]
