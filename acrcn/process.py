9875692f1fe5b43dcc6b5292ead0ec5417517306
Prannay Khosla <prannay.khosla@gmail.com>
2017-07-31 12:01:33 +0000

    Adding relevant images

--- acrcn/process.py
+++ acrcn/process.py
@@ -23,7 +23,7 @@ for folder in folders[:1] :
 		frames = np.zeros([30, 84, 64, 3])
 		for i,img in enumerate(images) : 
 			im = np.array(Image.open("%s/%s/%s"%(path, filename, img)).getdata())
+			im = (im / 255. ).resize([84,64, 3])
 			print(im)
-			im = (im / 255. ).resize([84,64])
 			frames[i] = im
 		np.save("%s/%s/video.npy"%(path, filename), frames)
