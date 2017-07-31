09accd085dfef073200c3f5f42cca48ae1c867da
Prannay Khosla <prannay.khosla@gmail.com>
2017-07-31 11:35:36 +0000

    Adding relevant images

--- acrcn/process.py
+++ acrcn/process.py
@@ -7,10 +7,10 @@ from subprocess import call
 folders = ["boxing", "handclapping", "handwaving", "jogging", "running", "walking"]
 direc = "/media/hdd/hdd/prannayk/action_reaction/"
 
-for folder in folders : 
+for folder in folders[:1] : 
 	path = direc + folder
 	filelist = [f for f in os.listdir(path) if isfile(join(path, f))]
-	for file in filelist:
+	for file in filelist[:10]:
 		filename = file.split("/")[-1].split(".")[0]
 		call(["mkdir",join(path, filename)])
 		os.system("ffmpeg -i %s/%s.avi -vf fps=5 -f image2 %s/%s/%s-"%(path, filename, path, filename, filename) + "%03d.jpg ")
