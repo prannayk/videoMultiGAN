bf8cacf9e71e42e8686963cc4331afd5a3af9fff
Prannay Khosla <prannay.khosla@gmail.com>
2017-08-01 14:15:40 +0000

    Adding relevant images

--- acrcn/process.py
+++ acrcn/process.py
@@ -8,10 +8,10 @@ from PIL import Image
 folders = ["boxing", "handclapping", "handwaving", "jogging", "running", "walking"]
 direc = "/media/hdd/hdd/prannayk/action_reaction/"
 
-for folder in folders : 
+for folder in folders[:1] : 
 	path = direc + folder
 	filelist = [f for f in os.listdir(path) if isfile(join(path, f))]
-	for file in filelist:
+	for file in filelist[:5]:
 		filename = file.split("/")[-1].split(".")[0]
 		call(["mkdir",join(path, filename)])
 		os.system("ffmpeg -i %s/%s.avi -vf fps=5 -s 40x32 -f image2 %s/%s/%s-"%(path, filename, path, filename, filename) + "%03d.png ")
@@ -28,4 +28,4 @@ for folder in folders :
 			print(im)
 			frames[i] = im
 		np.save("%s/video_%s.npy"%(direc, filename), frames)
-		os.system("rm -rf %s/%s")
+		# os.system("rm -rf %s/%s")
