d315d14c82c9d23004e832a54db747d2dd3585ee
Prannay Khosla <prannay.khosla@gmail.com>
2017-08-03 10:45:09 +0000

    Adding relevant images

--- acrcn/process.py
+++ acrcn/process.py
@@ -14,7 +14,7 @@ for folder in folders :
 	for file in filelist:
 		filename = file.split("/")[-1].split(".")[0]
 		call(["mkdir",join(path, filename)])
-		os.system("ffmpeg -i %s/%s.avi -vf fps=10 -s 40x32 -f image2 %s/%s/%s-"%(path, filename, path, filename, filename) + "%03d.png ")
+		os.system("ffmpeg -i %s/%s.avi -vf fps=5 -s 40x32 -f image2 %s/%s/%s-"%(path, filename, path, filename, filename) + "%03d.png ")
 		os.system("mv %s/%s-* %s/"%(path, filename, filename))
 		print("Done with %s"%(filename))
 		path_file = direc + folder + "/" + filename
@@ -27,5 +27,5 @@ for folder in folders :
 			im = (im / 255. ).reshape([32,40, 3])
 			print(im)
 			frames[i] = im
-		np.save("%s/video_10_%s.npy"%(direc, filename), frames)
+		np.save("%s/video_5_%s.npy"%(direc, filename), frames)
 		os.system("rm -rf %s/%s")
