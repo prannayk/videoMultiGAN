22f026724675d694de97015dfc49bf415762096d
Prannay Khosla <prannay.khosla@gmail.com>
2017-08-13 17:42:15 +0000

    Adding relevant images

--- acrcn/step_2_gen.py
+++ acrcn/step_2_gen.py
@@ -471,9 +471,10 @@ def train_epoch(flag=False, initial=True):
 			run += batch_size
 			feed_dict = {
 				placeholders['image_input'] : feed_list[0],
-				placeholders['x'] : feed_list[1],
-				placeholders['image_class_input'] : feed_list[2],
-				placeholders['text_label_input'] : feed_list[3],
+				placeholders['x_old'] : feed_list[1],
+				palceholders['x'] : feed_list[2]
+				placeholders['image_class_input'] : feed_list[3],
+				placeholders['text_label_input'] : feed_list[4],
 				placeholders['z_s'] : np.random.normal(0,1,[batch_size*frames, embedding_size]),
 				placeholders['z_c'] : random_label(batch_size*frames, num_class_image),
 				placeholders['z_t'] : np.random.normal(0,1,[batch_size*frames, num_class_motion])
@@ -521,9 +522,9 @@ for ep in range(epoch):
 	feed_list = gan.generate_batch()
 	feed_dict = {
 		placeholders['image_input'] : image_sample,
-		placeholders['x'] : image_gen,
-		placeholders['image_class_input'] : image_labels,
-		placeholders['text_label_input'] : text_labels,
+		placeholders['x'] : fe,
+		placeholders['image_class_input'] : feed_list[2],
+		placeholders['text_label_input'] : feed_list[3],
 		placeholders['z_s'] : np.random.normal(0,1,[batch_size*frames, embedding_size]),
 		placeholders['z_c'] : random_label(batch_size*frames, num_class_image),
 		placeholders['z_t'] : np.random.normal(0,1,[batch_size*frames, num_class_motion])
