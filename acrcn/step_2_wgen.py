cf84e47a66ae91a2a89cc5d9598747cc737f61a2
Prannay Khosla <prannay.khosla@gmail.com>
2017-08-22 09:44:55 +0000

    Adding relevant images

--- acrcn/step_2_wgen.py
+++ acrcn/step_2_wgen.py
@@ -510,6 +510,7 @@ def train_epoch(flag=False, initial=True):
 				_, loss_val[6] = session.run([optimizers["generator"], losses["generator_image"]], feed_dict=feed_dict)
 				_, loss_val[4] = session.run([optimizers["encoder"], losses["encoder"]], feed_dict=feed_dict)
 				_, loss_val[5] = session.run([optimizers["text_encoder"], losses["text_encoder"]], feed_dict=feed_dict)
+				_, loss_val[6]
 			else:
 				_, loss_val[6] = session.run([optimizers["generator_gan"], losses["generator_image"]], feed_dict=feed_dict)
 		count += 1
