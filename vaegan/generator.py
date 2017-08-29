6fc40f5a7c9fb5363cf57e550e6076b2fde1d045
Prannay Khosla <prannay.khosla@gmail.com>
2017-08-29 17:01:28 +0530

    Adding relevant images

--- vaegan/generator.py
+++ vaegan/generator.py
@@ -1,5 +1,5 @@
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/media/hdd/hdd/data_backup/prannayk/MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 from PIL import Image
 import numpy as np
 
