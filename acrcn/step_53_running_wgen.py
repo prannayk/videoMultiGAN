4cef77b383caa31701b0e2ab134a79b56326c8df
Prannay Khosla <prannay.khosla@gmail.com>
2017-09-01 01:19:28 +0530

    Adding relevant images

--- acrcn/step_53_running_wgen.py
+++ acrcn/step_53_running_wgen.py
@@ -8,7 +8,7 @@ from generator import rot_generator as generate
 # mnist = input_data.read_data_sets("/media/hdd/hdd/data_backup/prannayk/MNIST_data/", one_hot=True)
 
 total_size = 64000
-batch_size = 64
+batch_size = 16
 
 class VAEGAN():
 	"""docstring for VAEGAN, no parent class"""
@@ -30,9 +30,9 @@ class VAEGAN():
 		self.zdimension = self.num_class
 		self.motion_size = motion_size
 		self.learning_rate = map(lambda x: float(x), learning_rate[:(len(learning_rate) - 2)])
-		self.lambda_1 = 0
-		self.lambda_2 = 0
-		self.gan_scale = 10
+		self.lambda_1 = 300
+		self.lambda_2 = 200
+		self.gan_scale = 30
 		self.dim_1 = [self.image_shape[0], self.image_shape[1]]
 		self.dim_2 = [self.image_shape[0] // 2, self.image_shape[1] // 2]
 		self.dim_4 = [self.image_shape[0] // 4, self.image_shape[1] // 4]
@@ -504,7 +504,6 @@ saver = tf.train.Saver()
 merged = tf.summary.merge_all()
 train_writer = tf.summary.FileWriter("../logs/%s/"%(sys.argv[-2]))
 tf.global_variables_initializer().run()
-saver.restore(session, "/extra_data/prannay/models/large_acnrcn.ckpt")
 print("Running code: ")
 
 epoch = int(sys.argv[-1])
@@ -535,5 +534,5 @@ for e in range(epoch):
 	save_visualization(np.concatenate([image_sample, images],axis=3), save_path="../results/acrcn/32/%s/sample_%d.jpg"%(sys.argv[-2], ep+1))
 	summary = session.run(merged, feed_dict=feed_dict)
 	train_writer.add_summary(summary, ep)
-	saver.save(session, "/extra_data/prannay/trained_models/large_acnrcn_2.ckpt")
+	saver.save(session, "/extra_data/prannay/trained_models/large_lower_acnrcn_2.ckpt")
 
