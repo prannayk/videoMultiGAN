3ba95065e9c104703bb996e4e4a3a222e28832af
Prannay Khosla <prannay.khosla@gmail.com>
2017-09-01 16:01:07 +0530

    Adding relevant images

--- acrcn/step_53_wgen_eval.py
+++ acrcn/step_53_wgen_eval.py
@@ -500,7 +500,7 @@ def create_sprite_image(images):
 			if this_filter < images.shape[0]:
 				this_img = images[this_filter]
 				spriteimage[i*img_h:(i+1) * img_h,
-				j * img_w:(j + 1) * img_w ] = this_img
+				j * img_w:(j + 1) * img_w ] = this_img.reshape([32,40])
 	imsave("/users/gpu/prannay/vgan/sprite/test1.txt", spriteimage)
 
 def one_hot(X):
