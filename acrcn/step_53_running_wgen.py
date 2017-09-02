26ac939fa5d45bee1efd33eaa84a618e08d20534
Prannay Khosla <prannay.khosla@gmail.com>
2017-09-03 04:42:55 +0530

    Adding relevant images

--- acrcn/step_53_running_wgen.py
+++ acrcn/step_53_running_wgen.py
@@ -30,9 +30,9 @@ class VAEGAN():
 		self.zdimension = self.num_class
 		self.motion_size = motion_size
 		self.learning_rate = map(lambda x: float(x), learning_rate[:(len(learning_rate) - 2)])
-		self.lambda_1 = 10
+		self.lambda_1 = 300
 		self.iter = 0
-		self.lambda_2 = 5
+		self.lambda_2 = 200
 		self.gan_scale = 50
 		self.dim_1 = [self.image_shape[0], self.image_shape[1]]
 		self.dim_2 = [self.image_shape[0] // 2, self.image_shape[1] // 2]
